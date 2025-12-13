bef54cb9ae9e64e883a0ff8cf8993eb3f5b33633
Raju-02-19 <k.raju2k3@gmail.com>
2025-12-13 23:49:41 +0530

    Fix: added app.app_context() for OTP mail

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+ALLOWED_STUDENTS_CACHE = None
 # ---------------- Excel Loader ----------------
 
 
